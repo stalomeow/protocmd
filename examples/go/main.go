db03b30547dc2baf7a928fd90fc257310982a3a9
stalomeow <stalowork@163.com>
2023-12-01 15:27:30 +0800

    Update README

--- examples/go/main.go
+++ examples/go/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/stalomeow/protocmd"
-	"github.com/stalomeow/protocmd/examples/go/protos" // load all protos
+	"github.com/stalomeow/protocmd/examples/go/protos" // load all generated protos
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
@@ -11,20 +12,29 @@ func main() {
 	fmt.Printf("Load %v cmd\n", protocmd.CmdCount())
 	fmt.Println(protocmd.AllCmdIds())
 
-	// Get cmd name by id
-	fmt.Println(protocmd.CmdName(protos.TestRsp_TransformInfo_CmdId))
+	// Get cmdName by id
+	if cmdName, ok := protocmd.CmdName(protos.TestRsp_TransformInfo_CmdId); ok {
+		fmt.Println(cmdName)
+	}
 
-	// Create cmd by id
-	msg, err := protocmd.NewMessageByCmdId(protos.TestReq_CmdId)
+	buf, err := proto.Marshal(&protos.TestReq{Uid: "123321"})
 	if err != nil {
 		fmt.Printf("Error %v", err)
 		return
 	}
 
-	// Get cmd id and name
-	fmt.Printf("Cmd (id: %v, name: %s)\n", msg.CmdId(), msg.CmdName())
+	// Create message by cmdId
+	msg, err := protocmd.NewMessageByCmdId(protos.TestReq_CmdId)
+	if err != nil {
+		fmt.Printf("Error %v", err)
+		return
+	}
+	err = proto.Unmarshal(buf, msg)
+	if err != nil {
+		fmt.Printf("Error %v", err)
+		return
+	}
 
-	req := msg.(*protos.TestReq)
-	req.Uid = "123321"
-	fmt.Println(req)
+	fmt.Printf("Cmd (id: %v, name: %s)\n", msg.CmdId(), msg.CmdName())
+	fmt.Println(msg.(*protos.TestReq))
 }
